e7aadc40a178f34ad8fb9a67600fbdebb951f46d
Beth Schofield <bethmschofield@gmail.com>
2017-11-13 17:38:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 function printBadges(list) {
   for (let i=0; i < list.length; i++) {
-    console.log(`Welcome ${list[i]}! You are employee ${i+1}`)
+    console.log(`Welcome ${list[i]}! You are employee #${i+1}`)
   }
   return list
 }
-
-
